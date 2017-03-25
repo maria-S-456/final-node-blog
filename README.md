869fe5de267ff12a88b9901db9db496f92a28dae
maria-S-456 <mars381pika@gmail.com>
2017-03-25 06:22:02 +0500

    correction

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A blogging app that demonstrates adding, deleting, editing, and posting to and from a database on a remote mongo database. All operations work, except editing through html page. Password and username have been removed.
 
 blog-test.js has a GET test that is not yet working. (passes even when wrong)
+
+UPDATE: blog-test is testing GET correctly now. gets timeout error if run without "mocha -t 10000 blog-test.js"
--- test/blog-test.js
+++ test/blog-test.js
@@ -16,7 +16,7 @@ it('adds two numbers together', function(){
 		assert(2 + 3 === 5);
 	});
 
-
+//WORKING
 //need to run mocha with "mocha -t 10000 blog-test.js" to avoid the timeout error
 it('should make a /GET request', function(done){
 	chai.request(app).get('/blog').end(function(err, res){
@@ -28,5 +28,11 @@ it('should make a /GET request', function(done){
 		//done(); this will always pass if done() is placed here
 	});
 
-
+it('should make a /POST request', function(done){
+	chai.request(app).get('/blog').send({'title':'My Adventures in Timbuktu', 'Author':'Steven Spielberg', 'Content':'As I arrived, the most fantastic thing happened...'})
+	.end(function(err, res){
+		res.should.have.status(200);
+		done();
+	})
+})
 });
